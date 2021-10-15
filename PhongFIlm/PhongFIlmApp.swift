3d47f72fa2c4175b7f54cb51bdcf9709ed42cfba
phonggit3699 <phamphong3699@gmail.com>
2021-10-15 19:54:35 +0700

    Initial Commit

---
+++ PhongFIlm/PhongFIlmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhongFIlmApp.swift
+//  PhongFIlm
+//
+//  Created by PHONG on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PhongFIlmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
